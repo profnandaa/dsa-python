b0e1fa08a3ee94fe916048cd26b2dfff43867d4c
@ProfNandaa <profnandaa@gmail.com>
2015-12-14 22:22:34 +0300

    More recursion examples

--- recursion/basics.py
+++ recursion/basics.py
@@ -18,3 +18,33 @@ Where:
     - f(n): any extra work done in the function
 
 '''
+
+# More examples
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(5))
+
+# Binary Search
+
+
+def binary_search(data, target, start, end):
+    if start > end:
+        return False
+    else:
+        mid = (start + end) // 2
+        if target == data[mid]:
+            return mid
+        elif target < data[mid]:
+            return binary_search(data, target, start, mid - 1)
+        return binary_search(data, target, mid + 1, end)
+
+
+a = [20, 30, 50, 60, 89, 93, 100, 120, 150]
+
+print(binary_search(a, 93, 0, len(a) - 1))
